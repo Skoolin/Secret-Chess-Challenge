2ebabc51b7d024c92bbd9427bd63748ba18b5321
Skolin <superskolin@googlemail.com>
2023-07-24 19:36:01 +0200

    transposition table +155 tokens

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -12,6 +12,31 @@ public class MyBot : IChessBot
 
     ulong[] RepetitionTable = new ulong[800];
 
+    // can save 4 tokens by removing this line and replacing `TABLE_SIZE` with `32768`
+    const ulong TABLE_SIZE = 32768;
+
+    /*
+     * Transposition Table
+     * 
+     * to insert: ulong zobrist, int depth, int eval, int type, Move m
+     * TranspositionTable[zobrist % TABLE_SIZE] = (zobrist, depth, eval, type, m);
+     * 
+     * to retrieve:
+     * (ulong TTzobrist, int TTdepth, int TTeval, int TTtype, Move TTm) = TranspositionTable[zobrist % TABLE_SIZE];
+     * 
+     * Types:
+     * 1: PV, exact evaluation
+     * 2: beta cutoff, lower bound
+     * 3: all node, upper bound
+     */
+    readonly (
+        ulong, // zobrist
+        int,   // depth
+        int,   // eval
+        int,   // type
+        Move
+        )[] TranspositionTable = new (ulong, int, int, int, Move)[TABLE_SIZE];
+
     void MakeMove(Move m)
     {
         // for repetition detection
@@ -30,13 +55,16 @@ public class MyBot : IChessBot
         return 0;
     }
 
-    void SortMoves(Span<Move> moves)
+    void SortMoves(Span<Move> moves, Move tableMove)
     {
         // TODO
     }
 
     int AlphaBeta(int depth, int alpha, int beta)
     {
+        ulong zobrist = board.ZobristKey;
+        ulong TTidx = zobrist % TABLE_SIZE;
+
         // check if time is up
         // if so, we return later anyways so we dont need to return here (-3 token)
         if (30 * timer.MillisecondsElapsedThisTurn > timer.MillisecondsRemaining)
@@ -66,20 +94,46 @@ public class MyBot : IChessBot
 
         // TODO check for draw by 50 move rule
 
+        // query transposition table
+        (ulong TTzobrist, int TTdepth, int TTeval, int TTtype, Move TTm) = TranspositionTable[TTidx];
+
+        bool isTableHit = TTzobrist == zobrist;
+
+        j = alpha; // save starting alpha to detect PV and all nodes
+
         // leaf node returns static eval
         if (depth == 0)
         {
-            int eval = Eval();
-            return eval;
+            if (isTableHit)
+                return TTeval;
+            i = Eval();
+            TranspositionTable[TTidx] = (zobrist, 0, i, 1, TTm);
+            return i;
         }
 
-        Move bestMove;
+        if(isTableHit && TTdepth >= depth)
+        {
+            switch (TTtype) {
+                case 1:
+                    return TTeval;
+                case 2:
+                    if (TTeval >= beta) return TTeval;
+                    break;
+                case 3:
+                    if (TTeval <= beta) return TTeval;
+                    break;
+            }
+        }
+
+        // TTm is now "best move"
+        // might consider removing this line?? probably not though
+        if (!isTableHit) TTm = Move.NullMove;
 
         // TODO search TT entry before generating moves? or too token expensive?
 
         if (!hasGeneratedMoves)
             board.GetLegalMovesNonAlloc(ref moves);
-        SortMoves(moves);
+        SortMoves(moves, TTm);
 
         // stalemate
         // will not be found in leaf nodes, is that fine?
@@ -94,11 +148,18 @@ public class MyBot : IChessBot
             if (done) return 0; // time is up!!
 
             if (i >= beta)
+            {
+                TranspositionTable[TTidx] = (zobrist, depth, i, 2, m);
                 return beta;
+            }
             if (i > alpha)
+            {
                 alpha = i;
+                TTm = m;
+            }
         }
 
+        TranspositionTable[TTidx] = (zobrist, depth, alpha, j == alpha ? 3 : 1, TTm);
         return alpha;
     }
 
@@ -117,7 +178,7 @@ public class MyBot : IChessBot
         while (!done)
         {
             int bestEval = -1000000;
-            SortMoves(moves);
+            SortMoves(moves, bestMove);
 
             foreach (Move m in moves)
             {
