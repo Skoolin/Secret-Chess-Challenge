c3630c03c470f74792874b13287e434d67a76cb8
Arseniy <93079612+codedeliveryservice@users.noreply.github.com>
2023-07-31 15:48:26 +0300

    Increase testing time

--- Tests/MateSearchTests.cs
+++ Tests/MateSearchTests.cs
@@ -7,7 +7,7 @@ namespace Tests;
 public class MateSearchTests
 {
     private const int SearchTimeMultiplier = 30;
-    private static readonly int[] Timings = { 150, 300, 500, 2000 };
+    private static readonly int[] Timings = { 150, 300, 750, 2250 };
 
     [TestCaseSource(nameof(GetTestCases))]
     public void ShouldFindMateInX(string fen, int mateIn)
