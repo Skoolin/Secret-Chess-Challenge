a9c543007a217cee701407e76434dafd0c9f0065
Skolin <superskolin@googlemail.com>
2023-08-26 21:29:06 +0200

    only re-search PV node with full window

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -275,19 +275,18 @@ public class MyBot : IChessBot
             if (
                 // full search in qsearch
                 inQSearch
+                || moveCount == 1
                 || (
                     // late move reductions
                     moveCount <= 5
                     || depth <= 2
                     || alpha < (score = -AlphaBeta(depth - (pvNode ? 2 : 1 + Math.ILogB(depth)), -alpha - 1, -alpha))
                     )
-                &&
-                    (
+                && 
                     // zero window search
-                    moveCount <= 1
-                    || alpha < (score = -AlphaBeta(depth - 1, -alpha - 1, -alpha))
+                    alpha < (score = -AlphaBeta(depth - 1, -alpha - 1, -alpha))
                     && score < beta
-                    )
+                    && pvNode
                 )
                     // full window search
                     score = -AlphaBeta(depth - 1, -beta, -alpha);
